337f13d454056d7cbc0fbe5441273114b3a30893
Rahan <wangleibniz01@gmail.com>
2021-08-31 15:29:39 +0800

    first commit

---
+++ numbers.js
@@ -0,0 +1,33 @@
+/****Primality Test****/
+
+//version 1
+// function isPrime(n) {
+//   if (n <= 1) {
+//     return false
+//   }
+
+//   for (var i = 2; i < n; i++) {
+//     if (n % i == 0) {
+//       return false
+//     }
+//   }
+//   return true
+// }
+
+//version 2 (improved)
+
+function isPrime(n) {
+  if (n <= 1) return false
+  if (n <= 3) return false
+
+  //this is checked so that we can skip
+  // middle five numbers in below loop
+  if (n % 2 == 0 || n % 3 == 0) return false
+
+  for (var i = 5; i * i <= n; i = i + 6) {
+    if (n % i == 0 || n % (i + 2) == 0) return false
+  }
+  return true
+}
+
+console.log(isPrime(97))
