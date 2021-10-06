b1646293e9db0a250266d06fb035a362ef094744
Absolu242 <wangleibniz01@gmail.com>
2021-10-06 13:34:18 +0800

    Least recently used caching

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { LFUCache } from "./Caching/LFUNode.js";
-import { deleteDuplicateInUnsortedSll } from "./LinkedList/DeleteDuplicate.js";
+import { LFUCache } from "./Caching/LFUCaching.js";
 import SinglyLinkedList from "./LinkedList/LinkedList.js";
 
 var sll1 = new SinglyLinkedList();
@@ -16,9 +15,9 @@ myLFU.set(1, 1)
 myLFU.set(2, 2)
 myLFU.set(3, 3);
 myLFU.get(1)
-console.log(myLFU)
-console.log(myLFU.get(1))
-console.log(myLFU.get(1))
-console.log(myLFU.get(1))
-console.log(myLFU.get(1))
-console.log(myLFU)
+// console.log(myLFU)
+// console.log(myLFU.get(1))
+// console.log(myLFU.get(1))
+// console.log(myLFU.get(1))
+// console.log(myLFU.get(1))
+// console.log(myLFU)
