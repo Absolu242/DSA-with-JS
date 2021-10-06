c478ee4df33afc4b3718a3e008ce179dae5b7eb2
Absolu242 <wangleibniz01@gmail.com>
2021-10-06 13:21:26 +0800

    Least frequently used caching

--- LinkedList/DeleteDuplicate.js
+++ LinkedList/DeleteDuplicate.js
@@ -1,16 +1,16 @@
 
 export function deleteDuplicateInUnsortedSll(sll){
-    let track =[]
+    let track ={}
 
     let temp = sll.head;
     let prev=null ;
 
     while(temp){
-        if(track.indexOf(temp.data) >=0){
+        if(track[temp.data]){
             prev.next= temp.next;
             sll.size--;
         }else{
-            track.push(temp.data);
+            track[temp.data] = true;
             prev = temp;
         }
         temp = temp.next;
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+import { LFUCache } from "./Caching/LFUNode.js";
 import { deleteDuplicateInUnsortedSll } from "./LinkedList/DeleteDuplicate.js";
 import SinglyLinkedList from "./LinkedList/LinkedList.js";
 
 var sll1 = new SinglyLinkedList();
- sll1.insert(1);
- sll1.insert(2);
- sll1.insert(7);
- sll1.insert(2);
- sll1.insert(8);
- console.log(sll1)
- deleteDuplicateInUnsortedSll(sll1)
+//  sll1.insert(1);
+//  sll1.insert(2);
+//  sll1.insert(7);
+//  sll1.insert(2);
+//  sll1.insert(8);
+//  console.log(sll1)
+//  deleteDuplicateInUnsortedSll(sll1)
 
+var myLFU = new LFUCache(5);
+myLFU.set(1, 1)
+myLFU.set(2, 2)
+myLFU.set(3, 3);
+myLFU.get(1)
+console.log(myLFU)
+console.log(myLFU.get(1))
+console.log(myLFU.get(1))
+console.log(myLFU.get(1))
+console.log(myLFU.get(1))
+console.log(myLFU)
