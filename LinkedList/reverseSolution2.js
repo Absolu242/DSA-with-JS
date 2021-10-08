e19fc1cb2cc4de79c3a91402c7e1e198c0041b1b
Absolu242 <wangleibniz01@gmail.com>
2021-10-08 09:45:06 +0800

    other soolution for revsered linkedlist

---
+++ LinkedList/reverseSolution2.js
@@ -0,0 +1,15 @@
+
+
+export function reverse(head ){
+    let currentNode = head; 
+    let prev = null;
+
+   while(currentNode){
+        let next = currentNode.next
+        currentNode.next = prev;
+        prev = currentNode;
+        currentNode = next
+    }
+
+    return prev;
+}
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { LFUCache } from "./Caching/LFUCaching.js";
 import SinglyLinkedList from "./LinkedList/LinkedList.js";
 
+import {reverse} from "./LinkedList/reverse.js";
+
 var sll1 = new SinglyLinkedList();
-//  sll1.insert(1);
-//  sll1.insert(2);
-//  sll1.insert(7);
-//  sll1.insert(2);
-//  sll1.insert(8);
+ sll1.insert(1);
+ sll1.insert(2);
+ sll1.insert(7);
+ sll1.insert(2);
+ sll1.insert(8);
 //  console.log(sll1)
-//  deleteDuplicateInUnsortedSll(sll1)
 
 var myLFU = new LFUCache(5);
 myLFU.set(1, 1)
